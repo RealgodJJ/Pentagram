4d44de3f4a05f596c219bb82a6bcac90710d823c
maaonan <xiaozhiqiu13@sina.com>
2018-09-23 21:01:29 +0800

    Fractal tree with different color.

--- fractal_tree.py
+++ fractal_tree.py
@@ -9,19 +9,27 @@ import turtle
 
 
 def draw_branch(branch_length):
+    pace = 15
+    turtle.width(3)
     if branch_length > 5:
+        if branch_length - 2 * pace < 5:
+            turtle.color('green')
+
         # 绘制右侧树枝
         turtle.forward(branch_length)
         turtle.right(20)
-        draw_branch(branch_length - 15)
+        draw_branch(branch_length - pace)
 
         # 绘制左侧树枝
         turtle.left(40)
-        draw_branch(branch_length - 15)
+        draw_branch(branch_length - pace)
 
         # 返回之前的树枝
+        turtle.color('brown')
+        turtle.penup()
         turtle.right(20)
         turtle.backward(branch_length)
+        turtle.pendown()
 
 
 def main():
