1045247cfe13bece2cbc8c0fc007211ed16cd33f
maaonan <xiaozhiqiu13@sina.com>
2018-09-25 16:57:41 +0800

    BMR add some abnormal.

--- calculator_bmr.py
+++ calculator_bmr.py
@@ -16,27 +16,36 @@ def main():
         # height = float(input('身高（cm）：'))
         # age = int(input('年龄：'))
         print('请输入以下信息，用空格分隔')
-        input_str = input('性别 体重（kg） 身高（cm） 年龄：\n')
-        str_list = input_str.split(' ')
-        gender = str_list[0]
-        weight = float(str_list[1])
-        height = float(str_list[2])
-        age = int(str_list[3])
+        try:
+            input_str = input('性别 体重（kg） 身高（cm） 年龄：\n')
+            str_list = input_str.split(' ')
+            gender = str_list[0]
+            weight = float(str_list[1])
+            height = float(str_list[2])
+            age = int(str_list[3])
 
-        if gender == '男':
-            bmr = 13.7 * weight + 5.0 * height - 6.8 * age + 66
-        elif gender == '女':
-            bmr = 9.6 * weight + 1.8 * height - 4.7 * age + 655
-        else:
-            bmr = -1
+            if gender == '男':
+                bmr = 13.7 * weight + 5.0 * height - 6.8 * age + 66
+            elif gender == '女':
+                bmr = 9.6 * weight + 1.8 * height - 4.7 * age + 655
+            else:
+                bmr = -1
 
-        if bmr != -1:
-            print('您的性别：{}\n体重：{}千克\n身高：{}厘米\n年龄：{}'.format(gender, weight, height, age))
-            print('您的基础代谢率: {}大卡'.format(bmr))
-        else:
-            print('暂不支持该性别')
+            if bmr != -1:
+                print('您的性别：{}\n体重：{}千克\n身高：{}厘米\n年龄：{}'.format(gender, weight, height, age))
+                print('您的基础代谢率: {}大卡'.format(bmr))
+            else:
+                print('暂不支持该性别')
 
-        yes_or_no = input('是否退出程序(Y/N)？')
+            yes_or_no = input('是否退出程序(Y/N)？')
+        # except TypeError:
+        #     print("输入的数据类型有误！")
+        except ValueError:
+            print("请输入正确的信息！")
+        except IndexError:
+            print("输入的信息过少！")
+        except:
+            print("程序有误！")
 
 
 if __name__ == '__main__':
